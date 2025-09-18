3573cbf72732a204bc665a8b2c75d950cdc5d342
Thecow-007 <biermandaniel65@gmail.com>
2025-09-18 19:49:34 -0400

    Default Admin User
    A default admin user is now created if it doesn't exist already. The username and password are found in the env file.

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ import MongoStore from 'connect-mongo';
 import mongoose from 'mongoose';
 import cookieParser from 'cookie-parser';
 import mainRouter from "./routers/router.js";
+import { connectDB, seedDataBase } from './config/database.js';
+
 
 
 dotenv.config();
@@ -20,51 +22,57 @@ initializePassport(passport);
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 export const PROJECT_ROOT = __dirname;
+const startServer = async () => {
+    const app = express();
 
-const app = express();
+    // Serve all public files in the public folder (must be done before mounting main routes)
+    app.use(express.static(path.join(PROJECT_ROOT, "public")));
 
-// Serve all public files in the public folder (must be done before mounting main routes)
-app.use(express.static(path.join(PROJECT_ROOT, "public")));
+    app.use(express.json());
+    app.use(express.urlencoded({ extended: true }));
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+    app.use(cookieParser());
 
-app.use(cookieParser());
+    app.use(session({
+        secret: process.env.SESSION_SECRET,
+        resave: false,
+        saveUninitialized: false,
+        store: MongoStore.create({
+            mongoUrl: process.env.MONGO_URL // MongoDB connection string
+        }),
+        cookie: {
+            maxAge: 1000 * 60 * 60 * 24 * 1, // Cookie expires in 1 day
+        }
+    }));
 
-app.use(session({
-    secret: process.env.SESSION_SECRET,
-    resave: false,
-    saveUninitialized: false,
-    store: MongoStore.create({
-        mongoUrl: process.env.MONGO_URL // MongoDB connection string
-    }),
-    cookie: {
-        maxAge: 1000 * 60 * 60 * 24 * 1, // Cookie expires in 1 day
+    app.use(passport.initialize());
+    app.use(passport.session());
+
+    // View engine setup
+    app.set("views", path.join(PROJECT_ROOT, "views"));
+    app.set("view engine", "ejs");
+
+    // Connect to the database:
+    try {
+        await mongoose.connect(process.env.MONGO_URL);
+        console.log('MongoDB connected successfully.');
+    } catch (error) {
+        console.error('MongoDB connection failed:', error.message);
+        process.exit(1);
     }
-}));
 
-app.use(passport.initialize());
-app.use(passport.session());
+    //Mount all of the routes to /
+    app.use("/", mainRouter);
 
-// View engine setup
-app.set("views", path.join(PROJECT_ROOT, "views"));
-app.set("view engine", "ejs");
+    const PORT = process.env.PORT || 2121;
+    app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
+    });
 
-// Connect to the database:
-try {
-    await mongoose.connect(process.env.MONGO_URL);
-    console.log('MongoDB connected successfully.');
-} catch (error) {
-    console.error('MongoDB connection failed:', error.message);
-    process.exit(1);
 }
 
-//Mount all of the routes to /
-app.use("/", mainRouter);
-
-const PORT = process.env.PORT || 2121;
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+// Connect to the database and then start the server
+connectDB().then(async () => {
+    await seedDataBase();
+    startServer();
 });
-
-
