18be95dc83ef43bba361bd246bf39c3faf483b0c
Robert Ohly <ohly0001@algonquinlive.com>
2026-01-23 10:59:02 -0500

    Refactored motd

--- public/js/motd.js
+++ public/js/motd.js
@@ -4,33 +4,35 @@ document.addEventListener('DOMContentLoaded', async function () {
   const redirectURL = document.querySelector("#motd-readmore");
   const dismissButton = document.querySelector("#dismiss-button");
 
+  function updateBanner(json) {
+    text.textContent = json.message;
+      if (json.redirect) {
+        redirectURL.classList.remove('hidden');
+        redirectURL.href = `/devlogs/${json.redirect}`;
+      } else {
+        redirectURL.classList.add('hidden');
+        redirectURL.href = `/devlogs/void.html`; //intentional
+      }
+    banner.classList.remove("hidden");
+  }
+
   const res = await fetch('/api/motd/pull', {
     method: 'GET',
     headers: { 'Content-Type': 'application/json' }
   });
 
   const json = await res.json();
-  text.textContent = json.message;
-
-  if (json.redirect) {
-    redirectURL.classList.remove('hidden');
-    redirectURL.href = `/devlogs/${json.redirect}`;
-  } else {
-    redirectURL.classList.add('hidden');
-    redirectURL.href = `/devlogs/void.html`; //intentional
-  }
-  banner.classList.remove("hidden");
+  updateBanner(json);
 
   dismissButton.addEventListener("click", () => {
     banner.classList.add("hidden");
   });
 
   const evtSource = new EventSource('/events');
-
   evtSource.addEventListener('motd', (event) => {
     try {
       const json = JSON.parse(event.data);
-      text.textContent = json.message;
+      updateBanner(json);
     } catch (err) {
       console.error('Error processing SSE message:', err);
     }
