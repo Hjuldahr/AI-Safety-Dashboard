91ea699adc37e87987f67a3b18bfc06aed9a4e96
Thecow-007 <biermandaniel65@gmail.com>
2026-02-17 17:25:54 -0500

    Fixed Logs linking

--- controllers/logController.js
+++ controllers/logController.js
@@ -378,6 +378,7 @@ const getPage = async (req, res) => {
     try {
         res.render("logs", {
             user: req.user,
+            deepLink: null
         });
     } catch (error) {
         console.error("Error fetching logs page:", error);
